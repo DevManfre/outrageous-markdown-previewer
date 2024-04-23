457bacd8283b79c3086794206f8704693c0a696b
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-23 11:25:44 +0200

    :heavy_plus_sign: Add bootstrap

---
+++ src/components/MarkdownEditor.jsx
@@ -0,0 +1,12 @@
+function MarkdownEditor() {
+    return (
+        <div className="row">
+            <div className="editor">
+                <h4>Markdown Editor</h4>
+                <textarea></textarea>
+            </div>
+        </div>
+    );
+}
+
+export default MarkdownEditor;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import './static/css/style.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import MarkdownEditor from './components/MarkdownEditor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <h1>Markdown Previewer</h1>
         <p>by <a href="https://github.com/DevManfre">DevManfre</a></p>
+
+        <MarkdownEditor />
     </React.StrictMode>
 );
