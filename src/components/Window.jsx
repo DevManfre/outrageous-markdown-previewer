1348240d09d248b61d81490a640b00fd05ea479e
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-23 14:22:05 +0200

    :sparkles: Added textarea resize

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,13 +1,38 @@
+import autosize from "autosize";
+import { useEffect, useState } from "react";
+
 function Window({ children }) {
-    let isEditor = children == 'editor';
+    let isEditor = children === 'editor';
+
+    const [editorText, setEditorText] = useState(`## This is some markdown
+### Consider making your own
+    
+#### List items
+- George
+ Paul
+- Ringo
+- John
+    
+#### Make it **bold** or make it *italic*
+    
+#### Create links [Github](https://github.com/devmanfre)`);
+
+    function handleEditorInput(event) {
+        setEditorText(event.target.value);
+        autosize(event.target)
+    }
 
     return (
         <div className="col">
             <h4>
-                {isEditor ? <i class="bi bi-file-code"> </i> : <i class="bi bi-file-earmark"> </i>}
+                {isEditor ? <i className="bi bi-file-code"> </i> : <i className="bi bi-file-earmark"> </i>}
                 {children.charAt(0).toUpperCase() + children.slice(1)}
             </h4>
-            {isEditor ? <textarea id='editor' /> : <input type="text" />}
+            <textarea id={children}
+                value={editorText}
+                onInput={isEditor ? handleEditorInput : undefined}
+                readOnly={!isEditor}
+            />
         </div>
     );
 }
