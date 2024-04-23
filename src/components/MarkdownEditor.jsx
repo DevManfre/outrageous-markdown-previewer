1315be67e01e00761fd339e57ae014ac2033304f
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-23 14:38:37 +0200

    :art: Restructured code

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,10 +1,41 @@
-import Window from "./Window";
+import autosize from "autosize";
+import { useState } from "react";
 
 function MarkdownEditor() {
+    const [editorText, setEditorText] = useState(`## This is some markdown
+### Consider making your own
+    
+#### List items
+- George
+ Paul
+- Ringo
+- John
+    
+#### Make it **bold** or make it *italic*
+    
+#### Create links [Github](https://github.com/devmanfre)`);
+    const [previewText, setPreviewText] = useState('');
+
+    function handleEditorChange(event) {
+        setEditorText(event.target.value);
+        autosize(event.target);
+        setPreviewText(event.target.value);
+    }
+
+    function handlePreviewChange(event) {
+        autosize(event.target)
+    }
+
     return (
         <div className="row markdown-container">
-            <Window>editor</Window>
-            <Window>preview</Window>
+            <div className="col">
+                <h4><i className="bi bi-file-code"> </i>Editor</h4>
+                <textarea id='editor' value={editorText} onInput={handleEditorChange} />
+            </div>
+            <div className="col">
+                <h4><i className="bi bi-file-earmark"> </i>Preview</h4>
+                <textarea id='preview' value={previewText} readOnly onChange={handlePreviewChange} />
+            </div>
         </div>
     );
 }
